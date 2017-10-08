b912bd33611c011d57994163495d8d02d39b414d
alex-rokabilis <alekos_l2@hotmail.com>
2017-10-08 22:11:12 +0300

    include pigpio

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 var ip = require("ip");
+var Gpio = require('pigpio').Gpio;
 
+var motor = new Gpio(18, {mode: Gpio.OUTPUT})
 
 
 app.listen(3001, ip.address(), () => console.log(`listening on http://${ip.address()}:3001`))
@@ -24,7 +26,10 @@ function handler(req, res) {
 
 io.on('connection', function (socket) {
 
-    setInterval(() => socket.emit('servo_pulse_width::get', 1500), 1000)
+    setInterval(() => socket.emit('servo_pulse_width::get', motor.getServoPulseWidth()), 1000)
 
-    socket.on('servo_pulse_width::set', data => console.log(data));
+    socket.on('servo_pulse_width::set', data => {
+        data = parseInt(data);
+        if(data >=  500 && data <= 2500) motor.servoWrite(data)
+    });
 });
